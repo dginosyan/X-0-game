c5b85d0b459f1ed6939d3ce0019063dbb6bd008c
Давид Гиносян <davidginosan@Davids-MacBook-Air.local>
2021-07-14 03:31:51 +0400

    Refactor code

--- main.py
+++ main.py
@@ -5,91 +5,93 @@
 
 
 def game():
+    print("""
+    Добро пожаловать в игру «Крестики-нолики».
+    Для игры нужно два игрока.
+    Введите кординаты в формате, например 11.
+    ------------------------------------------
+    """)
+
     gamer_one = input("Игрок 1(Введите Имя): ")
     gamer_two = input("Игрок 2(Введите Имя): ")
 
     board = [
-        ["_","_","_"],
-        ["_","_","_"],
-        ["_","_","_"],
+        ["_", "_", "_"],
+        ["_", "_", "_"],
+        ["_", "_", "_"],
     ]
 
-    showBoard(board)
+    __show_board(board)
+    __gamer_actions_handler(board, gamer_one, gamer_two)
 
+
+def __gamer_actions_handler(board, gamer_one, gamer_two):
     step = 0
     while True:
-        if step % 2 == 0:
-            action = input("Ход игрока 1(%s): " % gamer_one)
-            x, y = map(int, list(action))
-            if (x in range(0,3) and y in range(0,3)) and board[x][y] == "_":
-                board[x][y] = 'X'
-            else:
-                showBoard(board)
-                print("Wrong cordinates: try it again")
-                continue
-        else:
-            action = input("Ход игрока 2(%s): " % gamer_two)
-            x, y = map(int, list(action))
-            if (x in range(0,3) and y in range(0,3)) and board[x][y] == "_":
-                board[x][y] = '0'
-            else:
-                showBoard(board)
-                print("Wrong cordinates: try it again")
-                continue
+        gamer_name = gamer_one if step % 2 == 0 else gamer_two
+        action = input("Ход игрока (%s): " % gamer_name)
+        try:
+            coordinate_x, coordinate_y = map(int, list(action))
+            print(coordinate_x, coordinate_y, (coordinate_x not in range(0, 3) and coordinate_y not in range(0, 3)) and board[coordinate_x][coordinate_y] != "_")
+            if (coordinate_x not in range(0, 3) or coordinate_y not in range(0, 3)) or board[coordinate_x][coordinate_y] != "_":
+                raise ValueError
+        except ValueError:
+            __show_board(board)
+            print("Некоректные кординаты: введите коректные кординаты в формате %s%s")
+            continue
+
+        board[coordinate_x][coordinate_y] = 'X' if step % 2 == 0 else '0'
+
         step += 1
-        showBoard(board)
-        result = checkIfWinnerExist(board)
+        __show_board(board)
+        result = __check_if_winner_exist(board)
         if result == "X":
-            print("Winner is %s" % gamer_one)
+            print("Победитель - %s" % gamer_one)
             break
         elif result == "0":
-            print("Winner is %s" % gamer_two)
+            print("Победитель - %s" % gamer_two)
             break
-        elif result is False:
+        elif result:
+            print("Ничья!!!")
             break
 
 
-def checkIfWinnerExist(board):
-    def checkSet(l):
-        if len(l) == 1 and l[0] != "_":
-            return l[0]
+def __check_if_winner_exist(board):
+    def check_set(board_coordinate_value_list):
+        coordinate_value_set = list(set(board_coordinate_value_list))
+        if len(coordinate_value_set) == 1 and coordinate_value_set[0] != "_":
+            return coordinate_value_set[0]
+
     for row in board:
-        result = checkSet(list(set(row)))
+        result = check_set(row)
         if result is not None:
             return result
 
     for column in range(3):
-        colSet = list(set([colVal[column] for colVal in board]))
-        result = checkSet(colSet)
+        result = check_set([colVal[column] for colVal in board])
         if result is not None:
             return result
 
-    c1 = checkSet(list(set([board[0][0], board[1][1], board[2][2]])))
-    c2 = checkSet(list(set([board[0][2], board[1][1], board[2][0]])))
-    # print(c1, c2, [board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]], "C1C2")
+    c1 = check_set([board[0][0], board[1][1], board[2][2]])
+    c2 = check_set([board[0][2], board[1][1], board[2][0]])
 
     if c1 is not None:
         return c1
     if c2 is not None:
         return c2
 
-    status = True
+    is_all_coordinates_filled = True
     for row in board:
         if "_" in row:
-            status = False
+            is_all_coordinates_filled = False
             break
 
-    if status:
-        print("Nichia")
-        return False
+    return is_all_coordinates_filled
 
 
-
-
-def showBoard(board):
-    print("Board:")
+def __show_board(board):
+    print("Доска игры:")
     print(" ", 0, 1, 2)
-
     for item in enumerate(board):
         print(item[0], *item[1])
 
